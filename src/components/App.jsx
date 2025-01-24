8cc317ed418ce77fe85c77754e41d96ec0ed8c91
Oleksandr Maksymenko <machinery69@gmail.com>
2025-01-23 21:08:59 -0500

    changed for Clay

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,7 @@ import MainCard from './MainCard';
 export const App = () => {
   return (
     <>
-      <h1 style={{ fontStyle: 'italic', paddingLeft: 20 }}>
-        {' '}
-        Щотижневик "Вісник компресорщини"
-      </h1>
+      
       <MainCard />;
     </>
   );
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -6,14 +6,8 @@ import Box from '@mui/system/Box';
 export default function MainCard() {
   return (
     <Card variant="outlined" sx={{ margin: 5, backgroundColor: 'lightblue' }}>
-      <Typography level="h1">Чи знали ви що?</Typography>
-      <Typography level="h2" fontSize="xl" sx={{ mb: 0.5 }}>
-        10 липня
-      </Typography>
-      <Typography fontSize="xl" sx={{}}>
-        В цей день народилась визначна постать українського суспільства, один з
-        найвеличніших хокеїстів сьогодення
-      </Typography>
+      <Typography level="h1">FUCK YOU!</Typography>
+      
 
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         {/* <AspectRatio> */}
@@ -27,26 +21,7 @@ export default function MainCard() {
           alt=""
         />
       </Box>
-      <Typography fontSize="xl">
-        Лауреат міжнародних конкурсів майстрів на всі руки
-      </Typography>
-
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        {/* <AspectRatio> */}
-        <img
-          style={{
-            width: '80%',
-            padding: '10px',
-          }}
-          src={require('../img/image2.jpeg')}
-          loading="lazy"
-          alt=""
-        />
-      </Box>
-      <Typography level="h1" sx={{ color: 'red' }}>
-        Редакція щотижневика вітає нашого співвітчизника, та бажає щастя,
-        міцного здоровля, та мирного неба!
-      </Typography>
+      
 
       {/* </AspectRatio> */}
     </Card>
